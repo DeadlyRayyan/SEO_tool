89a4332c47dc79319d415f03743f8e3d4cf7a9bd
rayyanshikoh <rayyanshikoh@icloud.com>
2021-05-10 20:35:32 +0400

    initial commit

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 import requests
 from keys import api_key
+from datetime import datetime
 
-keyword = 'talbina'
+keyword = 'shami kebab'
 maxResults = 20
 
 def statistics(videoId, api_key):
@@ -10,12 +11,20 @@ def statistics(videoId, api_key):
     video_info_array = video_info.json()
     return video_info_array
 
+class Keywords():
+    def __init__(self, keyword, num):
+        self.keyword = keyword
+        self.num = num
+
 class Video():
     def __init__(self, videoId):
         self.videoId = videoId
         self.head = self.head()
         self.tag = self.video_tags()
         self.view = self.view_count()
+        self.days = self.days()
+        self.score = self.calculate_score()
+
 
     def view_count(self):
         video_view_array = statistics(videoId, api_key)
@@ -28,6 +37,15 @@ class Video():
         video_title = video_view_array["items"][0]["snippet"]["title"]
         return str(video_title)
 
+    def days(self):
+        video_view_array = statistics(videoId, api_key)
+        days = video_view_array["items"][0]["snippet"]["publishedAt"]
+        days = days[:10]
+        date_time_obj = datetime.strptime(days, '%Y-%m-%d')
+        today = datetime.today()
+        days = today - date_time_obj
+        return int(str(days.days))
+
     def video_tags(self):
         # tags = requests.get(
         #     f"https://www.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&id={videoId}&key={api_key}")
@@ -39,10 +57,16 @@ class Video():
             video_tags = 'N/A'
         return video_tags
 
+    def calculate_score(self):
+        score = self.view/self.days
+        return score
+
 
 video = requests.get(
     f"https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults={maxResults}&q={keyword}&type=video&key={api_key}&order=relevance")
 video_list = video.json()
+
+print('Processing...')
 
 x = 0
 video_container = []
@@ -52,14 +76,34 @@ while x != maxResults:
     video_container.append(Video(videoId))
     x += 1
 
-# v = video_container[0]
-# print(v.videoId)
-# print(v.head)
-# print(v.view)
-# print(v.tag)
+video_container.sort(key=lambda x: x.score, reverse=True)
 
-video_container.sort(key=lambda x: x.view, reverse=True)
+# for y in video_container:
+#     print(f'{y.head}, https://www.youtube.com/watch?v={y.videoId}, {y.view}, {y.score}')
 
-for y in video_container:
-    print(f'{y.head}, https://www.youtube.com/watch?v={y.videoId}, {y.view}')
+keywords = []
+for a in video_container[:5]:
+    for words in a.tag:
+        work = words
+        work = work.split(',')
+        keywords.append(work)
 
+keywords_unique = []
+
+for words in keywords:
+    if words in keywords_unique:
+        pass
+    else:
+        keywords_unique.append(words)
+
+keys_container = []
+
+for unique in keywords_unique:
+    unique_key = str(unique)
+    num = keywords.count(unique)
+    keys_container.append(Keywords(unique_key, num))
+
+keys_container.sort(key=lambda x: x.num, reverse=True)
+
+for keyword in keys_container:
+    print(f'{keyword.keyword} occurs {keyword.num} times')
